dbdd2c9f6d7e99a77c66d35e0b8add1f2c1d8a05
vothuc2k3 <114125898+vothuc2k3@users.noreply.github.com>
2024-09-28 22:01:58 +0700

    Restructure poll data structure

---
+++ lib/features/activity_log/repository/activity_log_repository.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:hash_balance/core/constants/firebase_constants.dart';
+import 'package:hash_balance/core/failures.dart';
+import 'package:hash_balance/core/providers/firebase_providers.dart';
+import 'package:hash_balance/models/activity_log_model.dart';
+
+final activityLogRepositoryProvider = Provider((ref) {
+  return ActivityLogRepository(
+    firestore: ref.read(firebaseFirestoreProvider),
+  );
+});
+
+class ActivityLogRepository {
+  final FirebaseFirestore _firestore;
+
+  ActivityLogRepository({
+    required FirebaseFirestore firestore,
+  }) : _firestore = firestore;
+
+  CollectionReference get _activityLogs =>
+      _firestore.collection(FirebaseConstants.activityLogsCollection);
+
+  Future<Either<Failures, void>> addActivityLog(
+      ActivityLogModel activityLog) async {
+    try {
+      await _activityLogs.doc(activityLog.id).set(activityLog.toMap());
+      return right(null);
+    } on FirebaseException catch (e) {
+      return left(Failures(e.message!));
+    } catch (e) {
+      return left(Failures(e.toString()));
+    }
+  }
+
+  Stream<List<ActivityLogModel>> getActivityLog({required String uid}) {
+    return _activityLogs
+        .where('uid', isEqualTo: uid)
+        .orderBy('createdAt', descending: true)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) =>
+                ActivityLogModel.fromMap(doc.data() as Map<String, dynamic>))
+            .toList());
+  }
+}
